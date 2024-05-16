01670256db5eaee97ba93637fe3499a5703c897f
Sarah Rollins <s.rollins751@gmail.com>
2024-05-17 00:09:26 +0900

    Integrated search function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState, Suspense } from 'react';
-// import React { useState, Suspense } from 'react';
-import Status from './components/Status';
-// const Status = React.lazy(async () => {
-//   await new Promise(resolve => setTimeout(resolve, 10000));
-//   return import('./components/Status');
-// });
+// import { useState, Suspense } from 'react';
+import React, { useState, Suspense } from 'react';
+// import Status from './components/Status';
+const Status = React.lazy(async () => {
+  await new Promise(resolve => setTimeout(resolve, 2000));
+  return import('./components/Status');
+});
 import StatList from './components/StatusList';
 
 export interface QueryProps {
@@ -12,12 +12,29 @@ export interface QueryProps {
 }
 
 const App = () => {
+  const [searchText, setSearchText] = useState('')
   const [status, setStatus] = useState('200');
   const [dogs, setDogs] = useState (false);
 
   const displayAllDogs = () => {
     setDogs(!dogs);
   }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setSearchText(value);
+  };
+
+  const handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if(event.keyCode === 13) {
+      setStatus(searchText);
+    }
+  };
+
+  const handleSearch = () => {
+    setStatus(searchText);
+  };
+
 
   return (
     <div className="white-bg">
@@ -29,8 +46,8 @@ const App = () => {
         <Status q={status} />
       </Suspense>
       <div className="searchbar center">
-        <input placeholder="Enter HTTP status code" type='text' />
-        <button>Search</button>
+        <input onChange={handleChange} onKeyUp={handleKeyUp} placeholder="Enter HTTP status code" value={searchText} type='text' />
+        <button onClick={handleSearch}>Search</button>
       </div>
       {dogs && <StatList />}
     </div>

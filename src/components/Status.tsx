2b03913c6c637d9564f4bd542a1ec05133387aec
Sarah Rollins <s.rollins751@gmail.com>
2024-05-17 01:01:51 +0900

    Added not found image for search

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react';
 import { QueryProps } from '../App';
+import statusArray from '../StatusArray';
 
 const Status: FC<QueryProps> = ({ q }) => {
   return (
     <div className="center">
-      <img className="dogstat-img-main" src={`https://http.dog/${q}.jpg`} alt="http status code dog"/>
+      <img className={statusArray.includes(q) ? "dogstat-img-main" : "notfound-img"} src={statusArray.includes(q) ? `https://http.dog/${q}.jpg` : "src/assets/images/nodogsfound.png" } alt="http status code dog"/>
     </div>
   );
 };
